57d5854992dacfe77ceb247ac237b6e3546af6c0
yuhao <yuhao8888@gmail.com>
2020-08-25 16:42:44 +0800

    add host support

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var short = require('../models/short');
+const title = "Short-url service by yuhao";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Short-url', result: '', url: '' });
+  res.render('index', { title: title, result: '', url: '' });
 });
 
 /* POST */
 router.post('/', function(req, res, next){
+  let host = req.host;
+  if(host === "localhost") host += ":3000";
   short(req.param('url_input', null), function(url){
     if(url !== null)
-      res.render('index', {title: 'Short-url', result: 'short-url created:', url: 'http://localhost:3000/r/'+url});
+      res.render('index', {title: title, result: 'short-url created:', url: 'http://'+ host +'/r/'+ url});
     else
-      res.render('index', {title: 'Short-url', result: 'url pattern not match, please retry.', url: ''});
+      res.render('index', {title: title, result: 'url pattern not match, please retry.', url: ''});
   });
 });
 
