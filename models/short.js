176ec06c939e27e71e6e77f655059b8c35ab2b1b
yuhao <yuhao8888@gmail.com>
2020-08-25 16:12:45 +0800

    add regex for url

--- models/short.js
+++ models/short.js
@@ -6,30 +6,35 @@ client.on("error", function(error){
 });
 
 function generate_short(input, callback){
-    // TODO check input valid
     console.log("input: ", input.toString());
-    // TODO check already in db
-    client.get(input.toString(), function(err, result){
-        let random = null;
-        if(err){
-            console.log(err);
-        }
-        console.log("get: ", result);
-        if(result !== null && result !== undefined){
-            random = result;
-        }
-        else{
-            random = Math.random().toString(36).substr(2,7);
-            client.set(input.toString(), random, function(err, res){
-                if(err) console.log(err);
-            });
-            client.set(random, input.toString(), function(err, res){
-                if(err) console.log(err);
-                console.log("New key added.");
-            });
-        }
-        console.log("return: ", random);
-        callback(random);
-    });
+    let reg = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+    if(reg.test(input.toString())){
+        console.log("url correct.");
+        client.get(input.toString(), function(err, result){
+            let random = null;
+            if(err){
+                console.log(err);
+            }
+            console.log("get: ", result);
+            if(result !== null && result !== undefined){
+                random = result;
+            }
+            else{
+                random = Math.random().toString(36).substr(2,7);
+                client.set(input.toString(), random, function(err, res){
+                    if(err) console.log(err);
+                });
+                client.set(random, input.toString(), function(err, res){
+                    if(err) console.log(err);
+                    console.log("New key added.");
+                });
+            }
+            console.log("return: ", random);
+            callback(random);
+        });
+    }
+    else{
+        callback(null);
+    }
 }
 module.exports=generate_short;
--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,10 @@ router.get('/', function(req, res, next) {
 /* POST */
 router.post('/', function(req, res, next){
   short(req.param('url_input', null), function(url){
-    res.render('index', {title: 'Short-url', result: 'short-url created:', url: 'http://localhost:3000/r/'+url});
+    if(url !== null)
+      res.render('index', {title: 'Short-url', result: 'short-url created:', url: 'http://localhost:3000/r/'+url});
+    else
+      res.render('index', {title: 'Short-url', result: 'url pattern not match, please retry.', url: ''});
   });
 });
 
